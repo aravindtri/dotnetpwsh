d30f2d1a25a64e69a709bacb8f5c0f0d3459441b
aravindtri <aravindtri@gmail.com>
2023-03-03 14:52:00 -0500

    Initial commit

---
+++ powershellrunner/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Management.Automation;
+
+
+using (PowerShell ps = PowerShell.Create())
+{
+    ps.AddScript("pwd");
+    var pipelineObjects = await ps.InvokeAsync().ConfigureAwait(false);
+    foreach (var item in pipelineObjects)
+    {
+        Console.WriteLine(item.BaseObject.ToString());
+    }
+}
+
